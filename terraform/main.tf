451502cdeb08d885b81d81b039c0ed070fe91d06
Ki Hong <ki.hong@RM-MERASO-MAC.local>
2019-07-21 13:41:58 -0700

    ansible & terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,11 +50,11 @@ resource "null_resource" "ProvisionRemoteHostsIpToAnsibleHosts" {
 
 resource "null_resource" "ModifyApplyAnsiblePlayBook" {
   provisioner "local-exec" {
-    command = "sed -i -e '/hosts:/ s/: .*/: ${var.dev_host_label}/' play.yml"   #change host label in playbook dynamically
+    command = "sed -i -e '/hosts:/ s/: .*/: ${var.dev_host_label}/' ../ansible/play.yml"   #change host label in playbook dynamically
   }
 
   provisioner "local-exec" {
-    command = "sleep 10; ansible-playbook -i hosts play.yml"
+    command = "sleep 10; ansible-playbook -i hosts ../ansible/play.yml"
   }
   depends_on = ["null_resource.ProvisionRemoteHostsIpToAnsibleHosts"]
 }
